498abfe2c5e720bec4d6ab7d6998725d6f8a0c8e
Jacek Szulc <pa23143@student.wsb.gdynia.pl>
2022-04-01 22:57:54 +0200

    Operatory matematyczne

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int a = 17;
+        int b = 4;
+        int c = a + b; //=21
+        System.out.println(c);
+        c = a - b;     //=13
+        System.out.println(c);
+        c = a * b;     //=68
+        System.out.println(c);
+        c = a / b;     //=4 ponieważ 4*4=16 i zostaje reszty 1
+        System.out.println(c);
+        c = a % b;     //=1 reszta z dzielenia
+        System.out.println(c);
     }
 }
